9d68da30abd587d704ef72117fac79f5e0eae16b
arnold <luizarnoldch@gmail.com>
2025-10-02 13:10:44 -0500

    responsive the ladning page

--- src/app/(landing)/__components/LandingGallery/index.tsx
+++ src/app/(landing)/__components/LandingGallery/index.tsx
@@ -23,7 +23,7 @@ type LandingGalleryProps = {
 
 const LandingGallery = ({ lastCourse, totalEscuelas, totalCursos }: LandingGalleryProps) => {
   return (
-    <div className="max-w-6xl w-full flex justify-center md:flex-row gap-8">
+    <div className="max-w-6xl w-full flex flex-col md:flex-row justify-center gap-8">
       <div className="w-full md:w-1/2 aspect-[4/3]">
         <LastCourseCard course={lastCourse} />
       </div>
